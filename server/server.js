0fcdf09538e95ea816ddc4adce79d192e7c2bade
phantom-blaq <phantom.blaq00@gmail.com>
2025-12-22 17:47:37 +0530

    Sign/Login Implemented

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./mongoDB.js";
-const app = express();
-import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import protect from "./middleware/auth.js";
+
 dotenv.config();
 
+const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", protect, authRoutes);
 
 connectDB();
 app.listen(process.env.PORT, () => {
-    console.log(`Hello!! listening on port ${process.env.PORT}`)
-})
+  console.log(`🚀 Server running at http://localhost:${process.env.PORT}`);
+});
