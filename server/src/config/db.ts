f55a54321e86cd4868a582afccd7755ed6090800
phantom-blaq <phantom.blaq00@gmail.com>
2026-02-06 22:33:39 +0530

    Project Setup and User Schema and controller defined

---
+++ server/src/config/db.ts
@@ -0,0 +1,17 @@
+// src/config/db.ts
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected ✅");
+  } catch (error) {
+    console.error("MongoDB connection failed ❌", error);
+    process.exit(1);
+  }
+};
